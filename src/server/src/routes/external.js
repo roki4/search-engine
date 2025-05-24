85bd98dda97a5efd249ab0a33cf758f4ff6722af
Mark Gavrilov <mark.gavrilov.2018@list.ru>
2025-05-25 00:13:26 +0300

    new free api key

--- src/server/src/routes/external.js
+++ src/server/src/routes/external.js
@@ -57,7 +57,7 @@ router.get('/ai', validate(aiSchema), async (req, res) => {
       },
       {
         headers: {
-          Authorization: 'Bearer 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',
+          Authorization: 'Bearer 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',
           'Content-Type': 'application/json',
           'HTTP-Referer': 'http://localhost:3000',
           'X-Title': 'Quirk Search',
